e3d8d3c5c93958cc929b9a97b9727b3986fd7a16
Anton Bakalov <abakalov@calder.cs.umass.edu>
2010-04-12 18:22:36 -0400

    Added a simple cost function.

--- src/gpqo/Join.java
+++ src/gpqo/Join.java
@@ -15,8 +15,10 @@ public class Join extends Gene {
 	
 	public static String joinTypes = "SBH";
 	
-	public Join() {
-		
+	public Join() {}
+	
+	public Join(int joinId){
+		this.joinId = joinId;
 	}
 	
 	//Copy constructor
@@ -47,7 +49,71 @@ public class Join extends Gene {
 		this.outer = outer;
 	}
 
-
+	public double[] cost(){
+		
+		double innerCost = 0, innerSize = 0, outerCost = 0, outerSize = 0;
+		double[] returnCostAndSize = new double[2];
+		
+		if (inner instanceof Relation){
+			innerSize = ((Relation)inner).size;
+			innerCost = 0;
+		}
+		else{
+			double[] costAndSize = ((Join)inner).cost();
+			innerCost = costAndSize[0];
+			innerSize = costAndSize[1];
+		}
+			
+		if (outer instanceof Relation){
+			outerSize = ((Relation)outer).size;
+			outerCost = 0;
+		}
+		else{
+			double[] costAndSize = ((Join)outer).cost();
+			outerCost = costAndSize[0];
+			outerSize = costAndSize[1];
+		}
+		
+		// Assuming a reduction factor of 10. 
+		double reductionFactor = 10;
+		returnCostAndSize[0] = calcCost(innerSize, outerSize) + innerCost + outerCost;
+		returnCostAndSize[1] = Math.ceil(innerSize * outerSize / reductionFactor); 
+		
+		return returnCostAndSize;
+	}
+	
+	protected double calcCost(double innerSize, double outerSize){
+		// Assuming buffer size of 1002
+		double bufferSize = 1002, cost = -1;
+		
+		switch (joinType){
+			case 'S': 
+				double ratioNtoBInner = Math.ceil(innerSize / bufferSize);
+				double ratioNtoBOuter = Math.ceil(outerSize / bufferSize);
+				
+				cost = 2 * innerSize * (Math.ceil(Math.log(ratioNtoBInner) / Math.log(bufferSize - 1)) + 1) + 
+					2 * outerSize * (Math.ceil(Math.log(ratioNtoBOuter) / Math.log(bufferSize - 1)) + 1) + 
+					(innerSize + outerSize);
+				break;
+			
+			case 'B': 
+				double smallerRel = Math.min(innerSize, outerSize);
+				double biggerRel = Math.max(innerSize, outerSize);
+				
+				cost = smallerRel + biggerRel * Math.ceil(smallerRel / (bufferSize - 2)); 
+				break;
+			
+			case 'H': 
+				cost = 3 * (innerSize + outerSize);
+				break;
+			
+			default: 
+				System.out.println("Invalid join type!"); 
+				break;
+		}
+		
+		return cost;
+	}
 
 	public ArrayList<Join> pOrder(){
 		
